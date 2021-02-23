ae74ac760a145357a3269935d5851a5b6f66581a
tuutti <tuutti@iki.fi>
2021-02-23 14:36:12 +0200

    UHF-225: Views listing, tests for lists

--- tests/src/Kernel/MigrationTest.php
+++ tests/src/Kernel/MigrationTest.php
@@ -6,7 +6,7 @@ namespace Drupal\Tests\helfi_hauki\Kernel;
 
 use Drupal\helfi_hauki\Entity\Resource;
 use Drupal\Tests\helfi_api_base\Kernel\MigrationTestBase;
-use GuzzleHttp\Psr7\Response;
+use Drupal\Tests\helfi_hauki\Traits\MigrateTrait;
 
 /**
  * Tests hauki migrations.
@@ -14,6 +14,8 @@ use GuzzleHttp\Psr7\Response;
  * @group helfi_hauki
  */
 class MigrationTest extends MigrationTestBase {
+
+  use MigrateTrait;
 
   /**
    * {@inheritdoc}
@@ -37,59 +39,11 @@ class MigrationTest extends MigrationTestBase {
   }
 
   /**
-   * Create the resource migration.
-   *
-   * @return array
-   *   List of entities.
-   */
-  protected function createResourceMigration() : array {
-    $responses = [
-      new Response(200, [], json_encode(['count' => 20, 'results' => []])),
-    ];
-
-    $id = 1;
-    for ($page = 1; $page <= 2; $page++) {
-      $response = [
-        'count' => 20,
-        'results' => [],
-      ];
-
-      for ($i = 1; $i <= 10; $i++) {
-        $response['results'][] = [
-          'id' => $id,
-          'name' => [
-            'fi' => 'Name fi ' . $id,
-            'en' => 'Name en ' . $id,
-            'sv' => 'Name sv ' . $id,
-          ],
-          'origins' => [
-            [
-              'data_source' => [
-                'id' => 'tprek',
-              ],
-              'origin_id' => 'miscinfo-' . $id * 10,
-            ],
-          ],
-        ];
-        $id++;
-      }
-      $responses[] = new Response(200, [], json_encode($response));
-    }
-
-    $this->container->set('http_client', $this->createMockHttpClient($responses));
-    $this->executeMigration('hauki_resource', [
-      'source' => [
-        'url' => 'https://hauki-test.oc.hel.ninja/v1/resource/?origin_id_exists=true&page_size=10',
-      ],
-    ]);
-    return Resource::loadMultiple();
-  }
-
-  /**
    * Tests service migration.
    */
   public function testServiceMigration() : void {
-    $entities = $this->createResourceMigration();
+    $this->createResourceMigration();
+    $entities = Resource::loadMultiple();
     $this->assertCount(20, $entities);
 
     foreach (['en', 'sv', 'fi'] as $langcode) {
